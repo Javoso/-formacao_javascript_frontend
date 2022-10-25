16d68348c5a26f771d9be46b27385eac5cfdd656
Javoso <lucasqueirozqxd@gmail.com>
2022-10-24 22:43:44 -0300

    Conclusão do primeiro curso da formação Javascript para FrontEnd da Alura.

--- Exercicios/Exercicios-1/desafio-script.js
+++
@@ -1,2 +0,0 @@
-
-console.log(document.querySelector('input[type=tel]'));
--- JavaScript para Web - Crie paginas dinamicas/aluramidi/js/main.js
+++ JavaScript para Web - Crie paginas dinamicas/aluramidi/js/main.js
@@ -1,9 +1,36 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();  
-}
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomComId(id) {
 
+    const obj = document.querySelector(id);
+
+    if (obj && obj.localName === 'audio') {
+        obj.play();
+    } else {
+        console.error("Element not found or is not a audio element");
+    }
+}
+
+function tocaSom() {
+    document.querySelector('#som_tecla_pom').play();
+}
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-console.log(listaDeTeclas);
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const id = tecla.classList.item(1);
+    const idAudio = `#som_${id}`;
+
+    tecla.onclick = function () {
+        tocaSomComId(idAudio);
+    }
+
+    tecla.onkeydown = function (event) {
+        if (event.code === 'Enter' || event.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
