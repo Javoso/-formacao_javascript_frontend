1d196b706001cc3af0c8351def00925f5b930f2a
Javoso <lucasqueirozqxd@gmail.com>
2022-10-27 01:25:45 -0300

    Desabilitando o click no botão de menos

--- JavaScript - Manipulando o DOM/Robotron-2000-projeto_inicial/js/index.js
+++ JavaScript - Manipulando o DOM/Robotron-2000-projeto_inicial/js/index.js
@@ -45,7 +45,7 @@ function manipularDados(operacao, controle) {
     const peca = controle.querySelector('[data-contador]');
 
     if (operacao === '+') {
-        peca.value = parseInt(peca.value) + 1;
+        peca.value = parseInd(peca.value) + 1;
     } else {
         peca.value = parseInt(peca.value) - 1;
     }
