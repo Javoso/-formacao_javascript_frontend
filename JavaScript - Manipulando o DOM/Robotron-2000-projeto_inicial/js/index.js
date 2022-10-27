c9afbc17e6635300e9304def2b8a827747f4e558
Javoso <lucasqueirozqxd@gmail.com>
2022-10-27 01:28:17 -0300

    Correção no desabitar o click no botão de menos

--- JavaScript - Manipulando o DOM/Robotron-2000-projeto_inicial/js/index.js
+++ JavaScript - Manipulando o DOM/Robotron-2000-projeto_inicial/js/index.js
@@ -45,9 +45,9 @@ function manipularDados(operacao, controle) {
     const peca = controle.querySelector('[data-contador]');
 
     if (operacao === '+') {
-        peca.value = parseInd(peca.value) + 1;
+        peca.value = parseInt(peca.value) + 1;
     } else {
-        peca.value = parseInt(peca.value) - 1;
+        peca.value = parseInd(peca.value) - 1;
     }
 }
 
